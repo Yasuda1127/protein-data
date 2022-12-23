c892f63792fe5207b74389e86f239059b6e0c4fc
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2022-12-23 16:36:47 +0900

    server.js変更

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+server.js
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const cors = require('cors')
+
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+
+const allowlist = [
+    'https://protein-flame.vercel.app',
+    'https://protein-65avm5fws-ronaudo9.vercel.app',
+    'https://protein-nu.vercel.app',
+    'https://protein-blue.vercel.app',
+]
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowlist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+server.use(cors(corsOptions))
 
 server.use(middlewares)
 // Add this before server.use(router)
